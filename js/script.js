9fc56b71493305e8975b7223147cbfc0f67e39b5
Nick Dziubynskyi <nick.dzub@gmail.com>
2022-07-13 20:09:50 +0300

    add function showdb and genres

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 "use strict";
 
-const numberOfMovies = +prompt("Number of films", "");
+let numberOfMovies;
+function start() {
+  numberOfMovies = +prompt("Number of movies you have watched", "");
+
+  while (
+    numberOfMovies == "" ||
+    numberOfMovies == null ||
+    isNaN(numberOfMovies)
+  ) {
+    numberOfMovies = +prompt("Number of movies you have watched", "");
+  }
+}
+
+start();
+
 const personalMovieDB = {
   count: numberOfMovies,
   movies: {},
@@ -9,32 +23,57 @@ const personalMovieDB = {
   privat: false,
 };
 
-if (personalMovieDB.count < 10) {
-  alert("You have watched too few movies");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-  alert("You are classic viewer");
-} else if (personalMovieDB.count >= 30) {
-  alert("You really like movies");
-} else {
-  alert("Error");
-}
-
-for (let i = 0; i < numberOfMovies; i++) {
-  const lastMovie = prompt("One of the last watched movie", ""),
-    stars = prompt("Rate this movie", "");
-  if (
-    lastMovie != null &&
-    stars != null &&
-    lastMovie != "" &&
-    stars != "" &&
-    lastMovie.length < 50
-  ) {
-    personalMovieDB.movies[lastMovie] = stars;
-    console.log("done");
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10) {
+    alert("You have watched too few movies");
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert("You are classic viewer");
+  } else if (personalMovieDB.count >= 30) {
+    alert("You really like movies");
   } else {
-    console.log("error");
-    i--;
+    alert("Error");
   }
 }
 
-console.log(personalMovieDB);
+// detectPersonalLevel();
+
+function rememberMyMovies() {
+  for (let i = 0; i < 2; i++) {
+    const lastMovie = prompt("One of the last watched movie", ""),
+      stars = prompt("Rate this movie", "");
+    if (
+      lastMovie != null &&
+      stars != null &&
+      lastMovie != "" &&
+      stars != "" &&
+      lastMovie.length < 50
+    ) {
+      personalMovieDB.movies[lastMovie] = stars;
+      console.log("done");
+    } else {
+      console.log("error");
+      i--;
+    }
+  }
+}
+
+// rememberMyMovies();
+
+function showMyDB(hidden) {
+  if (!hidden) {
+    console.log(personalMovieDB);
+  }
+}
+
+// showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+  for (let i = 0; i < 3; i++) {
+    personalMovieDB.genres[i] = prompt(
+      `Your favorite genre by number ${i + 1} is`
+    );
+  }
+}
+
+writeYourGenres();
+showMyDB(personalMovieDB.privat);
