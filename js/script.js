56325c091390e28d16b11e0c1495d3dd8a39cda3
Nick Dziubynskyi <nick.dzub@gmail.com>
2022-07-13 17:20:42 +0300

    add terms

--- js/script.js
+++ js/script.js
@@ -1,20 +1,40 @@
 "use strict";
 
-const numberOfFilms = +prompt("Number of films", "");
+const numberOfMovies = +prompt("Number of films", "");
 const personalMovieDB = {
-  count: numberOfFilms,
+  count: numberOfMovies,
   movies: {},
   actors: {},
   genres: [],
   privat: false,
 };
 
-const a = prompt("last film", ""),
-  b = prompt("stars", ""),
-  c = prompt("last film", ""),
-  d = prompt("stars", "");
+if (personalMovieDB.count < 10) {
+  alert("You have watched too few movies");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  alert("You are classic viewer");
+} else if (personalMovieDB.count >= 30) {
+  alert("You really like movies");
+} else {
+  alert("Error");
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < numberOfMovies; i++) {
+  const lastMovie = prompt("One of the last watched movie", ""),
+    stars = prompt("Rate this movie", "");
+  if (
+    lastMovie != null &&
+    stars != null &&
+    lastMovie != "" &&
+    stars != "" &&
+    lastMovie.length < 50
+  ) {
+    personalMovieDB.movies[lastMovie] = stars;
+    console.log("done");
+  } else {
+    console.log("error");
+    i--;
+  }
+}
 
 console.log(personalMovieDB);
