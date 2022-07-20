ceabe1496e923ac074b29c7f56b46ab28fe1de64
Nick Dziubynskyi <nick.dzub@gmail.com>
2022-07-20 12:40:21 +0300

    add sriptrec file with test info

---
+++ js/scriptRec.js
@@ -0,0 +1,78 @@
+let students = {
+  js: [
+    {
+      name: "John",
+      progress: 100,
+    },
+    {
+      name: "Ivan",
+      progress: 60,
+    },
+  ],
+  html: {
+    basic: [
+      {
+        name: "Peter",
+        progress: 20,
+      },
+      {
+        name: "Ann",
+        progress: 18,
+      },
+    ],
+    pro: [
+      {
+        name: "Sam",
+        progress: 10,
+      },
+    ],
+  },
+};
+
+// Перебор объекта через итерацию, высчитывание среднего арифметического
+function getTotalProgressByIteration(data) {
+  let total = 0;
+  let students = 0;
+
+  for (let course of Object.values(data)) {
+    if (Array.isArray(course)) {
+      students += course.length;
+      for (let i = 0; i < course.length; i++) {
+        total += course[i].progress;
+      }
+    } else {
+      for (let subCourse of Object.values(course)) {
+        students += subCourse.length;
+        for (let i = 0; i < subCourse.length; i++) {
+          total += subCourse[i].progress;
+        }
+      }
+    }
+  }
+  return total / students;
+}
+
+console.log(getTotalProgressByIteration(students));
+
+// перебор объекта рекурсией, высчитывание среднего арифметического
+function getTotalProgressByRecursion(data) {
+  if (Array.isArray(data)) {
+    let total = 0;
+    for (let i = 0; i < data.length; i++) {
+      total += data[i].progress;
+    }
+    return [total, data.length];
+  } else {
+    let total = [0, 0];
+    for (let subData of Object.values(data)) {
+      const subDataArray = getTotalProgressByRecursion(subData);
+      total[0] += subDataArray[0];
+      total[1] += subDataArray[1];
+    }
+    return total;
+  }
+}
+
+console.log(getTotalProgressByRecursion(students));
+const result = getTotalProgressByRecursion(students);
+console.log(result[0] / result[1]);
